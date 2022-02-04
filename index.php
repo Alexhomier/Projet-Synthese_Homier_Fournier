161e42ac25253861d8bad56ae7738733339d61fb
Alexhomier <a.homier@hotmail.com>
2022-02-03 20:38:08 -0500

    add login

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<html>
-  <head>
-    <title>Mathieu pu</title>
-  </head>
-  <body>
-    <h1>Mathieu pu le couscous!</h1>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>M.A. Simulation | Login</title>
+  <link rel="stylesheet" href="css/login.css">
+  <script src="js/login.js"></script>
+</head>
+<body>
+  <div class="background">
+    <form action="" method="POST">
+      
+    </form>
+  </div>
+</body>
 </html>
