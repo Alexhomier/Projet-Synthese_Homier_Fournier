41fd226b7d004ece7b26e4202e4dcb20c0151746
Alexhomier <a.homier@hotmail.com>
2022-02-04 09:46:34 -0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@
 
         if(!empty($data->action) && $data->action == "closed" &&
        !empty($data->pull_request) &&
-       $data->pull_request->base->ref == "master"){
-            shell_exec("git pull origin master");
+       $data->pull_request->base->ref == "main"){
+            shell_exec("git pull origin main");
             shell_exec("sudo systemctl restart apache2");
         }
