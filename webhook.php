d26c47cfa6ab032cb2689d88d6dd49b84bd2a2f5
Alexhomier <a.homier@hotmail.com>
2022-02-04 09:31:29 -0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,11 +1,9 @@
 <?php
     $data = json_decode($_POST['payload']);
-        echo "Erreur 404";
 
-        if(!empty($data->action) && $data->action == "closed" && 
-       !empty($data->pull_request) && 
+        if(!empty($data->action) && $data->action == "closed" &&
+       !empty($data->pull_request) &&
        $data->pull_request->base->ref == "master"){
-
-                shell_exec("git pull origin master"); 
-                echo "Script executed";
+            shell_exec("git pull origin master");
+            shell_exec("sudo systemctl restart apache2");
         }
