487a32940aa6019bc2cfe2e207d2e0b8ec1acf6b
Alexhomier <a.homier@hotmail.com>
2022-02-11 16:45:14 -0500

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,9 +1,3 @@
 <?php
-    $data = json_decode($_POST['payload']);
-
-        if(!empty($data->action) && $data->action == "closed" &&
-       !empty($data->pull_request) &&
-       $data->pull_request->base->ref == "main"){
-            shell_exec("git pull origin main");
-            shell_exec("sudo systemctl restart apache2");
-        }
+    shell_exec("git pull origin main");
+    shell_exec("sudo systemctl restart apache2");
