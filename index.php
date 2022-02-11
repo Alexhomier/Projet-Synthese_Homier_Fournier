e4e378a5c5248d71c83c8bdbc2a7076e2e105c77
Alexhomier <a.homier@hotmail.com>
2022-02-10 23:19:22 -0500

    ajout login

--- index.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <title>Mathieu pu</title>
-  </head>
-  <body>
-    <h1>Mathieu pu le couscous!</h1>
-  </body>
-</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>M.A. Simulation | Connexion</title>
+  <link rel="stylesheet" href="css/login.css">
+  <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@700&family=Roboto+Mono:wght@200&family=Work+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+  <div class="background">
+      <div class="background-ia-img"></div>
+      <header class="header">
+        <div class="header-container">
+          <a href="login.php" class="header-item-title">M.A.</a>
+        </div>
+      </header>
+      <div class="container-pres-title-text">
+        <div class="container-pres-title">
+          <h1 class="item-pres-title"></h1>
+        </div>
+        <div class="container-pres-text">
+          <h3 class="item-pres-text">
+            
+          </h3>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
