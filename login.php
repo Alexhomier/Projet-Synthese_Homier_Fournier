1086298531f4387d575819be5faeb41108d31d6b
Alexhomier <a.homier@hotmail.com>
2022-02-14 13:31:07 -0500

    Ajouts infos login

--- login.php
+++ login.php
@@ -10,14 +10,24 @@
   <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@700&family=Roboto+Mono:wght@200&family=Roboto:wght@100&family=Work+Sans&display=swap" rel="stylesheet"> 
 </head>
 <body>
-  <div class="background">
-      <div class="background-ia-img"></div>
+  <div class="pres-background">
+      <div class="pres-background-ia-img"></div>
       <header class="header">
         <div class="header-container">
           <a href="login.php" class="header-item-title">M.A.</a>
-          <div class="header-item-login" onclick="login()">Connexion</div>
+          <div class="header-item-link">
+            <a href="#" class="header-item-link-decoration">M.A.</a>
+            <p class="header-item-link-decoration">|</p>
+            <a href="#" class="header-item-link-decoration">Services</a>
+            <p class="header-item-link-decoration">|</p>
+            <a href="#" class="header-item-link-decoration">Contexte</a>
+            <p class="header-item-link-decoration">|</p>
+            <a href="#" class="header-item-link-decoration">CVM</a>
+          </div>
+          <div class="header-item-login" ><h2 class="header-item-login-decoration">Connexion</h2></div>
         </div>
       </header>
+      <!-- Section Pres -->
       <div class="container-pres">
         <div class="container-pres-title-text">
           <div class="container-pres-title">
@@ -29,7 +39,7 @@
               un moyen d'apprendre et de s'amuser en créant des dispositions 
               d'immeubles réelle ou tout droit sortie de leur imagination.
               Que ce soit pour un projet professionnelle ou personnel, 
-              notre équipe vous assure que nous sauront répondre à
+              notre équipe vous assure qu'elle saura répondre à
               vos besoins.
             </p>
           </div>
@@ -39,6 +49,10 @@
           <div class="item-pres-start-button"></div>
         </div>
       </div>
+    </div>
+    <!-- Section M.A. -->
+    <div class="ma-background">
+
     </div>
 </body>
 </html>
