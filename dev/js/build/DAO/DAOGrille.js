dc60819bf26fbf4e625b3dfd9d28dc9e0e469bca
Alexhomier <a.homier@hotmail.com>
2022-05-10 17:17:30 -0400

    Grille gestion erreur

--- dev/js/build/DAO/DAOGrille.js
+++ dev/js/build/DAO/DAOGrille.js
@@ -4,32 +4,37 @@ class DAOGrille {
     }
 
     sendToPy(isSave = false) {
-        document.querySelector(".loading").style.display = "flex";
-        let loading = document.querySelector(".loading-title");
-        if (isSave) {
-            loading.innerHTML = "Sauvegarde en cours...";
-        } else {
-            loading.innerHTML = "Chargement de la simulation en cours...";
-        }
-        fetch(`http://127.0.0.1:5000/post`, {
-                method: "post",
-                credentials: "omit",
-                body: JSON.stringify(this.dictValues),
-                headers: new Headers({
-                    "content-type": "application/json"
+        try {
+            document.querySelector(".loading").style.display = "flex";
+            let loading = document.querySelector(".loading-title");
+            if (isSave) {
+                loading.innerHTML = "Sauvegarde en cours...";
+            } else {
+                loading.innerHTML = "Chargement de la simulation en cours...";
+            }
+            fetch(`http://127.0.0.1:5000/post`, {
+                    method: "post",
+                    credentials: "omit",
+                    body: JSON.stringify(this.dictValues),
+                    headers: new Headers({
+                        "content-type": "application/json"
+                    })
                 })
-            })
-            .then(response => response.json())
-            .then(response => {
-                response = JSON.stringify(response);
-                localStorage.setItem('grille', response);
-                if (!isSave)
-                    window.location.href = "visualisation.php";
-                else {
-                    this.saveGridBD(response);
-                    // window.location.href = "leaderboard.php";
-                }
-            })
+                .then(response => response.json())
+                .then(response => {
+                    response = JSON.stringify(response);
+                    localStorage.setItem('grille', response);
+                    if (!isSave)
+                        window.location.href = "visualisation.php";
+                    else {
+                        this.saveGridBD(response);
+                        // window.location.href = "leaderboard.php";
+                    }
+                });
+        } catch (error) {
+            console.error("La console python est éteinte");
+        }
+
     }
 
     saveGridBD(grille) {
