0e5c4258e7b64204df0448889093920ce92b59c4
Alexhomier <a.homier@hotmail.com>
2022-05-13 22:42:19 -0400

    f

--- dev_Python/DAO.py
+++ dev_Python/DAO.py
@@ -17,7 +17,7 @@ def post():
     return jsonify(grille)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8500)
 
 # """
 #     run is the main of the algo.
