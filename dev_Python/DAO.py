e7955016b7c4ea0c1fbd1dda67d297cdc7105b40
Alexhomier <a.homier@hotmail.com>
2022-05-13 23:01:04 -0400

    Update DAO.py

--- dev_Python/DAO.py
+++ dev_Python/DAO.py
@@ -1,6 +1,6 @@
 from urllib import request
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from Manipulator import *
 
 app = Flask(__name__)
