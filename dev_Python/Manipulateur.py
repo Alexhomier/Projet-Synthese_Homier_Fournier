f55d155c8e5b43a86d103017596572b978df6f07
mathfournier <mathsoc@hotmail.com>
2022-05-24 21:35:51 -0400

    methode placer individus

--- dev_Python/Manipulateur.py
+++ dev_Python/Manipulateur.py
@@ -6,8 +6,6 @@ from Astar import *
 from ClassJson import *
 
 #
-#  Modifier les priority queue pour des lists et les sort en utilisant sorted() ou min() puisque l'ordre de la piority queue. 
-#  ^^ Cependant, est ce que c'est possible de faire cela avec des list de d'une valeur et d'un objet List = [[score, case], [score, case], ...]
 #  Vectoriser les arrays (numpy)
 #
 
@@ -18,42 +16,39 @@ from ClassJson import *
 class Manipulateur():
     def __init__(self, grille):
         self.__grille_size = len(grille["grille"])
-        self.__nb_ind_par_salle = grille["IndParSalle"]
+        self.__nb_indvidivu = grille["IndParSalle"]
         self.__minX = grille["minX"]
         self.__maxX = grille["maxX"]
         self.__minY = grille["minY"]
         self.__maxY = grille["maxY"]
-        self.__closest_end = PriorityQueue()  ## sort ou min a la place de Priority queue?
+        self.__closest_end = PriorityQueue()  
         self.__final_array = []
         self.__frames = []
 
         self.__grille = Traduction(grille["grille"], 1000, self.__grille_size)
 
-        #resultScale = Scaling(grille["grille"], 1000, self.__grille_size) self.__grille = resultScale[0]
-
-        resultWalls = GetWalls(self.__grille, self.__grille_size, self.__minX, self.__maxX, self.__minY, self.__maxY) #Gestion d'erreur s'il n'y a pas de porte extérieur
+        resultWalls = GetWalls(self.__grille, self.__grille_size, self.__minX, self.__maxX, self.__minY, self.__maxY)
         self.__grille = resultWalls[0]
         self.__end_array = resultWalls[1]
 
         self.__individu_array = []
-        resultIndividu = PlaceIndividus(self.__grille, self.__nb_ind_par_salle, self.__minX, self.__maxX, self.__minY, self.__maxY)
+        resultIndividu = PlaceIndividus(self.__grille, self.__nb_indvidivu, self.__minX, self.__maxX, self.__minY, self.__maxY)
         self.__grille = resultIndividu[0]
         for individu in resultIndividu[1]:
             self.__individu_array.append(individu[0])
         self.__initial_individus = self.__individu_array
         self.jsonIndividu = toJsonIndividu(self.__initial_individus)
-            
+
         self.__nb_in = len(self.__individu_array)
         self.__nb_out = 0
         self.__blocked = 0
 
     def _do_individus_frames(self):
-        if not self.__end_array == None:
+        if not self.__end_array == None:  #S'il n'y a pas de porte
             while self.__nb_out < self.__nb_in and self.__blocked < self.__nb_in-self.__nb_out:  #Si les individus sont tous bloqués, sa sort.
                 self.__grille = UpdateVoisinGrille(self.__grille, self.__minX, self.__maxX, self.__minY, self.__maxY, "Closest")
                 self.__individu_array = ChooseEnd(self.__grille, self.__end_array, self.__individu_array)
                 self.__closest_end = ClosestEnd(self.__individu_array, self.__grille)
-                self.__grille = UpdateVoisinGrille(self.__grille, self.__minX, self.__maxX, self.__minY, self.__maxY, "Algo")
                 self.__individu_array = [] 
                 self.__blocked_array = []
                 self.__blocked = 0
@@ -61,9 +56,8 @@ class Manipulateur():
                 frames_temp = []
                 while not self.__closest_end.empty():
                     current_individu = self.__closest_end.get()[2]
+                    self.__grille = UpdateVoisinGrille(self.__grille, self.__minX, self.__maxX, self.__minY, self.__maxY, "Algo") 
                     current_individu.update_voisins_algo(self.__grille)
-
-                    self.__grille = UpdateVoisinGrille(self.__grille, self.__minX, self.__maxX, self.__minY, self.__maxY, "Algo") ##Test TBH NOT CHANGING MUCH PROBABLY TAKING MORE TIME CAUSE OF THAT
 
                     if not len(current_individu.voisins) == 0: 
                         result = Astar.single_algo(None, self.__grille, current_individu, current_individu.get_end())
@@ -81,7 +75,7 @@ class Manipulateur():
                                 frames_temp.append(next_case)
                                 self.__nb_out += 1
                                 print(str(self.__nb_out) + " personne(s) out")
-                            elif current_individu != next_case:
+                            else :
                                 next_case.iden = current_individu.iden
                                 next_case.add_came_from(current_individu)
                                 next_case.type = "Individu"
@@ -92,16 +86,10 @@ class Manipulateur():
                                 self.__grille[pos_next[0]][pos_next[1]] = next_case
                                 self.__individu_array.append(next_case)
                                 frames_temp.append(next_case)
-                            else:
-                                print("Didn't move")
-                                self.__individu_array.append(current_individu)
-                                frames_temp.append(current_individu)
                         else:
-                            print("Blocked, Algo", current_individu.get_position())
                             self.__individu_array.append(current_individu)
                             frames_temp.append(current_individu)
                     else:
-                        print("Blocked, sans voisins", current_individu.get_position())
                         self.__individu_array.append(current_individu)
                         frames_temp.append(current_individu)
                         self.__blocked_array.append(current_individu)
